1149a30886d0b96cff418e3dfe247de9e0013e4a
Ashnal <ashnal52@gmail.com>
2019-12-22 00:21:33 -0500

    small fix to prevent errorspam

--- SRB2Kart (Mods).bat
+++ SRB2Kart (Mods).bat
@@ -4,4 +4,4 @@ git remote add origin https://github.com/casual-army/KartPackage.git
 git fetch --all
 git reset --hard origin/master
 powershell -c "if (-not (Test-Path .\textures.kart)) {Expand-Archive -DestinationPath . .\textures.zip}"
-start powershell -c $Args=@(); $Args+='-file';$Args+=gci .\mods\loadfirst\*;$Args+='bonuschars.kart';$Args+=gci .\mods\chars\*;$Args+=gci .\mods\tracks\*;$Args+=gci .\mods\music\*;$Args+=gci .\mods\loadlast\*;Start-Process srb2kart.exe -ArgumentList $Args
+powershell -c $Args=@(); $Args+='-file';$Args+=gci .\mods\loadfirst\*;$Args+='bonuschars.kart';$Args+=gci .\mods\chars\*;$Args+=gci .\mods\tracks\*;$Args+=gci .\mods\music\*;$Args+=gci .\mods\loadlast\*;Start-Process srb2kart.exe -ArgumentList $Args
