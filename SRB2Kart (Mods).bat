f15faa71a5204072f6074e9527a0fbc2447bff32
Ashnal <ashnal52@gmail.com>
2019-12-22 00:09:34 -0500

    Fix for batch

--- SRB2Kart (Mods).bat
+++ SRB2Kart (Mods).bat
@@ -1,6 +1,7 @@
 @echo off
 git init
-git remote add origin git@github.com:casual-army/KartPackage.git
-git pull
-powershell -c if (-not (Test-Path .\textures.kart)) {Expand-Archive -DestinationPath . .\textures.zip}
+git remote add origin https://github.com/casual-army/KartPackage.git
+git fetch --all
+git reset --hard origin/master
+powershell -c "if (-not (Test-Path .\textures.kart)) {Expand-Archive -DestinationPath . .\textures.zip}"
 start powershell -c $Args=@(); $Args+='-file';$Args+=gci .\mods\loadfirst\*;$Args+='bonuschars.kart';$Args+=gci .\mods\chars\*;$Args+=gci .\mods\tracks\*;$Args+=gci .\mods\music\*;$Args+=gci .\mods\loadlast\*;Start-Process srb2kart.exe -ArgumentList $Args
