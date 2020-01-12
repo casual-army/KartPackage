1b94324214e2e321d334d16d2c8a150a532379ee
Ashnal <ashnal52@gmail.com>
2020-01-12 04:11:06 -0500

    Fixed HI mapslot conflict between Daytona Hell and CNR Townsville *AGAIN*
    CNR Townsville was moved to slot HM and works now.
    Moved a few things with Barkpark, unhid MMBC Crystal, Hid MMBC South Pole

--- SRB2Kart (Mods).bat
+++ SRB2Kart (Mods).bat
@@ -1,3 +1,3 @@
 @echo off
 powershell -c "if (-not (Test-Path .\textures.kart)) {Expand-Archive -DestinationPath . .\textures.zip}"
-powershell -c $Args=@(); $Args+='-file';$Args+=gci .\mods\loadfirst\*;$Args+='bonuschars.kart';$Args+=gci .\mods\chars\*;$Args+=gci .\mods\tracks\*;$Args+=gci .\mods\music\*;$Args+=gci .\mods\loadlast\*;Start-Process srb2kart.exe -ArgumentList $Args
+powershell -c $Args=@(); $Args+='-opengl';$Args+='-file';$Args+=gci .\mods\loadfirst\*;$Args+='bonuschars.kart';$Args+=gci .\mods\chars\*;$Args+=gci .\mods\tracks\*;$Args+=gci .\mods\music\*;$Args+=gci .\mods\loadlast\*;Start-Process srb2kart.exe -ArgumentList $Args
